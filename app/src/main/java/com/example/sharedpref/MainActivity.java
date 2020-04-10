fe2ad5f14e2d6fbab16eaa957027f0a91818e747
Nancy <chemutain@gmail.com>
2020-04-10 17:43:04 +0300

    Add and display details in sharedpreferences

--- app/src/main/java/com/example/sharedpref/MainActivity.java
+++ app/src/main/java/com/example/sharedpref/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.sharedpref;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,8 +26,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayInfo(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences("userinfo", Context.MODE_PRIVATE);
+        String name = sharedPreferences.getString("Username","");
+        String pass = sharedPreferences.getString("Password", "");
+
+        txtdisplay.setText(name + " " + pass);
     }
 
     public void saveInfo(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences("userinfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor= sharedPreferences.edit();
+        editor.putString("Username", username.getText().toString());
+        editor.putString("Password", password.getText().toString());
+
+        editor.apply();
+
+        Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show();
     }
 }
